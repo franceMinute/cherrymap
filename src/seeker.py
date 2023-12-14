6b8b8a513d1460dde2039eb8feb1765343633ecc
franceMinute <42216990+franceMinute@users.noreply.github.com>
2023-12-14 00:54:40 -0500

    Remove cherrytree components

--- src/seeker.py
+++ src/seeker.py
@@ -1,12 +1,12 @@
 import argparse
 import logging
-from utils.dbUtils import create_connection
 from utils.loggingUtils import parse_log_level
+from parsers.nmapParser import parse_nmap_xml
 
 def main():
     # Argument parsing
     parser = argparse.ArgumentParser(description='Connect to an SQLite database.')
-    parser.add_argument('--db', help='Path to SQLite database file', required=True)
+    parser.add_argument('--vault', help='Path to Obsidian vault', required=True)
     parser.add_argument('--log-level', default='INFO', help='Set the logging level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)')
     args = parser.parse_args()
 
@@ -14,13 +14,9 @@ def main():
     log_level = parse_log_level(args.log_level)
     logging.basicConfig(level=log_level)
 
-    # Create a database connection
-    database = args.db
-    conn = create_connection(database)
+    tmp = parse_nmap_xml(args.vault)
 
-    # Close db connection
-    if conn:
-        conn.close()
+    logging.info("Done.")
 
 if __name__ == '__main__':
     main()
---
+++ src/utils/obsidianUtils.py
@@ -0,0 +1,9 @@
+import os
+import logging
+
+def generate_vault(vault_path):
+    try:
+        os.makedirs(vault_path)
+    except FileExistsError:
+        logging.info("File {} already exists. ".format(vault_path))
+        
